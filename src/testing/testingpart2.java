594a54d60f2048ce1b80fcce7f9eeade08b69847
Muniba <munibashara12@gmail.com>
2026-03-31 22:43:58 +0800

    Create testingpart2.java

---
+++ src/testing/testingpart2.java
@@ -0,0 +1,12 @@
+package testing;
+
+public class testingpart2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+
+	}
+
+}
