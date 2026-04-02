9f4c85d2469aeedd4726546e7e63b49919ae6c7f
Markma <224489317+Markmyr0421@users.noreply.github.com>
2026-04-02 09:56:16 +0800

    Extend SmokeBomb class from Item

--- SC2002 Combatants/SmokeBomb.java
+++ SC2002 Combatants/SmokeBomb.java
@@ -1,3 +1,11 @@
-public class SmokeBomb {
-    
+public class SmokeBomb extends Item {
+
+    SmokeBomb() {
+        super("SmokeBomb");
+    }
+
+    @Override
+    public void use(Player player, Combatant target) {
+        
+    }
 }
