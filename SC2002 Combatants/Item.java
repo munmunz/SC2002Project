5889ac8b9b5c32593b022e5bc00d98b62d315f12
Markma <224489317+Markmyr0421@users.noreply.github.com>
2026-04-02 09:55:21 +0800

    Change Item interface to abstract class with type field

--- SC2002 Combatants/Item.java
+++ SC2002 Combatants/Item.java
@@ -1,4 +1,13 @@
-public interface Item {
-    public String getType();
-    public void use(Player player, Combatant target);
+public abstract class Item {
+    private final String type;
+
+    Item(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public abstract void use(Player player, Combatant target);
 }
