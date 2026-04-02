e04eb5916ede67b1459a403fd6b01f4d40a7bd0c
Markma <224489317+Markmyr0421@users.noreply.github.com>
2026-04-02 09:55:57 +0800

    Update Potion.java

--- SC2002 Combatants/Potion.java
+++ SC2002 Combatants/Potion.java
@@ -1,24 +1,19 @@
-public class Potion implements Item {
-    private String type;
+public class Potion extends Item {
     int healAmount;
 
     Potion(){
-        this.type = "Potion";
+        super("Potion");
         this.healAmount = 100;
     }
 
     @Override
-    public String getType(){
-        return type;
-    }
-
-    @Override
     public void use(Player player, Combatant target){
-        int newHealthPoints = player.getHealthPoints() + 100;
+        int newHealthPoints = player.getHealthPoints() + healAmount;
         if(newHealthPoints > player.getMaxHealthPoints()){
             newHealthPoints = player.getMaxHealthPoints();
         }
         player.setHealthPoints(newHealthPoints);
     }
 }
+
 
