cc9065a9ab3b5d1d578b52e36b7933e2f0fac1d7
Markma <224489317+Markmyr0421@users.noreply.github.com>
2026-04-03 22:47:57 +0800

    Update Defend.java

--- SC2002 Combatants/Defend.java
+++ SC2002 Combatants/Defend.java
@@ -1,1 +1,29 @@
 
+public class Defend implements Action {
+	private Combatant user;
+	
+	public Defend(Combatant user) {
+		this.user = user;
+	}
+	
+	/* until status.java is completed
+	public boolean isValid(Combatant user)
+		// cannot defend again if defend status is still active during that round? unless its accumulative?
+		ArrayList<Status> statusList = user.getStatus();
+		for (int i = 0, i < statusList.size(); i++) {
+			Status s = statusList.get(i);
+			if (s instanceof DefendStatus) {
+				return false;
+			
+			}
+		return true;
+		}
+		
+	public void execute() {
+		DefendStatus defendStatus = new DefendStatus(user);
+		user.applyStatus(defendStatus);
+	}
+
+	*/
+
+}
