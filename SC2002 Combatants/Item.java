2cb25bf398b10b9f6a2bdf3ef9e255bab22d9555
Markma <224489317+Markmyr0421@users.noreply.github.com>
2026-04-03 22:42:49 +0800

    Update Item.java

--- SC2002 Combatants/Item.java
+++ SC2002 Combatants/Item.java
@@ -1,21 +1,6 @@
-<<<<<<< Updated upstream
 import java.util.ArrayList;
 
 public interface Item {
     String getType();
     void use(Player player, Combatant target, ArrayList<Combatant> enemies);
-=======
-public abstract class Item {
-    private final String type;
-
-    Item(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public abstract void use(Player player, Combatant target);
->>>>>>> Stashed changes
 }
