0527754a8737fc72c35c8b53f07fe03b4770512d
mikaelkaikai <mikaelkaikai@gmail.com>
2026-04-05 16:35:08 +0800

    Implement Status class hierarchy and subclasses

--- SC2002 Combatants/Status.java
+++ SC2002 Combatants/Status.java
@@ -1,5 +1,26 @@
-public class Status {
-    public void apply(){
-        return;
-    }    
+public abstract class Status {
+    protected String name;
+    protected int cooldown;
+    protected boolean applied;
+
+    public Status(String name, int cooldown) {
+        this.name = name;
+        this.cooldown = cooldown;
+        this.applied = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCooldown() {
+        return cooldown;
+    }
+
+    public boolean isApplied() {
+        return applied;
+    }
+
+    public abstract void apply();
+    public abstract void remove();
 }
