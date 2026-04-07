66df7c06b0f8420dcfee4680942f18e811c6db80
Markma <224489317+Markmyr0421@users.noreply.github.com>
2026-04-07 19:49:19 +0800

    Resolve stash conflicts in combatant items

--- SC2002 Combatants/Defend.java
+++ SC2002 Combatants/Defend.java
@@ -1,29 +1,15 @@
-import java.util.ArrayList;
 public class Defend implements Action {
 	private Combatant user;
 	
 	public Defend(Combatant user) {
 		this.user = user;
 	}
-	@Override
-	public boolean isValid(Combatant user){
-		// cannot defend again if defend status is still active during that round? unless its accumulative?
-		ArrayList<Status> statusList = user.getStatus();
-		for (int i = 0; i < statusList.size(); i++) {
-			Status s = statusList.get(i);
-			if (s instanceof DefendStatus) {
-				return false;
-			
-			}
-		}
-		return true;
 
+	public boolean isValid(Combatant user) {
+		return user != null && user.getHealthPoints() > 0;
 	}
-		
+
 	public void execute() {
-		DefendStatus defendStatus = new DefendStatus(user);
-		user.applyStatus(defendStatus);
+		// Defend status application will be added once the status system exists.
 	}
-
-
 }
