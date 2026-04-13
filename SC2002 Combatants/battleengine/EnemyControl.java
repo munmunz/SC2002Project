9a393225c35381c4db67d99ff576e5d06cfc2ac6
nathanongwe <51483690+nathanongwe@users.noreply.github.com>
2026-04-13 16:22:24 +0800

    Fix BattleEngine
    - Fixed TurnOrderStategy syntax + made method static
    - Add getter method for battlefield to get player
    - Made getWave in DifficultyLevel a static method

--- SC2002 Combatants/battleengine/EnemyControl.java
+++ SC2002 Combatants/battleengine/EnemyControl.java
@@ -1,44 +1,19 @@
 package battleengine;
 
-import java.util.ArrayList;
-
 import actions.Action;
-import actions.BasicAttack;
 import actions.Targetable;
 import core.BattleField;
-import core.Combatant;
 import core.Enemy;
 
 public class EnemyControl {
 
-    public void getEnemyMove(ArrayList<Action> actions) {
-        Enemy enemy = null;
-        for (Enemy aliveEnemy : BattleField.getAliveEnemies()) {
-            if (aliveEnemy.getActions() == actions) {
-                enemy = aliveEnemy;
-                break;
-            }
+    public static void getEnemyMove(Enemy enemy) {
+        Action move = (enemy.getActions()).get(0);
+
+        if (move instanceof Targetable){
+            ((Targetable)move).setTarget(BattleField.getPlayer());
         }
 
-        if (enemy == null) {
-            return;
-        }
-
-        Combatant player = null;
-        for (Combatant combatant : BattleField.getAliveCombatants()) {
-            if (!(combatant instanceof Enemy)) {
-                player = combatant;
-                break;
-            }
-        }
-
-        if (player == null) {
-            return;
-        }
-
-        Action enemyAction = new BasicAttack();
-        enemyAction.setUser(enemy);
-        ((Targetable) enemyAction).setTarget(player);
-        enemyAction.execute();
+        move.execute();
     }
 }
--- SC2002 Combatants/battleengine/TurnOrderStrategy.java
+++ SC2002 Combatants/battleengine/TurnOrderStrategy.java
@@ -6,8 +6,8 @@ import core.Combatant;
 
 public class TurnOrderStrategy {
 
-    public ArrayList<Combatant> roundOrder(ArrayList<Combatant> combatants) {
-        combatants.sort(Comparator.comparing(c -> c.getSpeed()))
+    public static ArrayList<Combatant> roundOrder(ArrayList<Combatant> combatants) {
+        combatants.sort(Comparator.comparing(c -> c.getSpeed()));
         return combatants;
 
     }
