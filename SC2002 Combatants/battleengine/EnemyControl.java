31e7519284e80d96742503b3146321bfd0793825
nathanongwe <51483690+nathanongwe@users.noreply.github.com>
2026-04-13 17:51:29 +0800

    Small Bug Fixes

--- SC2002 Combatants/battleengine/EnemyControl.java
+++ SC2002 Combatants/battleengine/EnemyControl.java
@@ -9,6 +9,7 @@ public class EnemyControl {
 
     public static void getEnemyMove(Enemy enemy) {
         Action move = (enemy.getActions()).get(0);
+        move.setUser(enemy);
 
         if (move instanceof Targetable){
             ((Targetable)move).setTarget(BattleField.getPlayer());
